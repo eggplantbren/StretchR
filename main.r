aef2e4cadc9f5004a0a8aa3ad3bdcd9670e00c4f
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-11-03 20:19:36 +1300

    Source correct file

--- main.r
+++ main.r
@@ -1,6 +1,6 @@
 # Main executable for affine-invariant sampling
 # This just targets the posterior using emcee-style stretch moves
-source('Model.R')
+source('model.r')
 
 numWalkers <- as.integer(100)
 if(numDimensions >= numWalkers)
