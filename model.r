c8c73baf2ae0d418a27c062ebb64e61a9e2af368
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-03-02 13:19:28 +1300

    Fixed bug

--- model.r
+++ model.r
@@ -1,5 +1,5 @@
 # This file contains functions that define what model we're working on.
-numDimensions <- as.integer(100)
+numDimensions <- as.integer(50)
 
 # Given a vector of parameters, evaluate the natural logarithm of the prior
 # probability density.
@@ -25,7 +25,7 @@ logLikelihood <- function(params)
 {
 	alpha = 0.95
 	logL <- -0.5*params[1]**2
-	logL = logL - 0.5*sum((params[2:numDimensions] - params[1:numDimensions-1])**2)/(1. - alpha**2)
+	logL = logL - 0.5*sum((params[2:numDimensions] - alpha*params[1:numDimensions-1])**2)/(1. - alpha**2)
 	return(logL)
 }
 
